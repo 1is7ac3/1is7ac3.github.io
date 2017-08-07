aeee958d3e8c1e583c3faed4ddbf6aaa885f6af4
1is7ac3 <isaac.qa13@gmail.com>
2017-08-07 16:49:23 -0400

    Update 07-08-2017

--- WebContent/js/script1.js
+++ WebContent/js/script1.js
@@ -1,4 +1,16 @@
 /**
  * 
  */
-alert("Prueba");
+var modal = document.getElementById('myModal');
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() { 
+    modal.style.display = "none";
+}
