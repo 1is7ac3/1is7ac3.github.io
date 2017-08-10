5640ee4e8aea9ba522a6df2ae4d80609ab4bead7
1is7ac3 <isaac.qa13@gmail.com>
2017-08-10 15:02:42 -0400

    Update 10-08-2017

--- WebContent/js/script_ficha.js
+++ WebContent/js/script_ficha.js
@@ -1,6 +1,8 @@
 var modal=document.getElementById('myModal');
 var text1=document.getElementById("text1");
 var text2=document.getElementById('text2');
+var text3=document.getElementById('text3');
+var text4=document.getElementById('text4');
 var modalImg=document.getElementById("img01");
 var captionText=document.getElementById("caption");
 var span=document.getElementsByClassName("close")[0];
@@ -20,6 +22,16 @@ text2.onclick=function(){
 	modal.style.display='block';
 	modalImg.src='document/certificado_2.png';
 	captionText.innerHTML='Certificado Activate';
+}
+text3.onclick=function(){
+	modal.style.display='block';
+	modalImg.src='document/certificado_3.png';
+	captionText.innerHTML='Certificado Activate';
+}
+text4.onclick=function(){
+	modal.style.display='block';
+	modalImg.src='document/certificado_4.png';
+	captionText.innerHTML='Certificado Platzi';
 }
 span.onclick = function() { 
     modal.style.display = "none";
