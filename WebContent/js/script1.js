a5da216454060a4f447ff4955e60a2849fd4db29
1is7ac3 <isaac.qa13@gmail.com>
2017-08-10 18:31:12 -0400

    Update 18:30

--- WebContent/js/script1.js
+++ WebContent/js/script1.js
@@ -1,45 +1,41 @@
 /**
- * 
- */
-var modal = document.getElementById('myModal');
-var img = document.getElementById('myImg');
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-var span = document.getElementsByClassName("close")[0];
+var modal=document.getElementById('myModal');
+var img=document.getElementById('myImg');
+var modalImg=document.getElementById("img01");
+var captionText=document.getElementById("caption");
+var span=document.getElementsByClassName("close")[0];
+*/
+class contacto{
+	constructor(name,number,icon){
+		this.name=name;
+		this.number=number;
+		this.icon=icon;
+	}
+}
+var contactos=[];
 var email=document.getElementById('email');
 var whatsapp=document.getElementById('whatsapp');
+contactos.push(new contacto(email,'isaac.qa13@gmail.com',email.innerHTML));
+contactos.push(new contacto(whatsapp,'+58 416-2824229',whatsapp.innerHTML));
+email.onmouseover=function(){show(contactos[0].number,contactos[0].name);}
+email.onmouseout=function(){hide(contactos[0].name,contactos[0].icon);}
+whatsapp.onmouseover=function(){show(contactos[1].number,contactos[1].name);}
+whatsapp.onmouseout=function(){hide(contactos[1].name,contactos[1].icon);}
 
-whatsapp.onmouseover=function()
-{
-	whatsapp.innerHTML='+58 416-2824229';
-	whatsapp.style.background="#F1B702";
-	whatsapp.style.cursor="text";
-	whatsapp.style.borderRadius="5px";
-	whatsapp.style.color="#000000";
+function show(number,name){
+	name.innerHTML=number;
+	name.style.cursor="text";
 }
-whatsapp.onmouseout=function(){
-	whatsapp.innerHTML='<img class="contacto" src="WebContent/icons/whatsapp.svg"/>';
-	whatsapp.style.cursor="pointer";
-	whatsapp.style.background="#F1B702";
+function hide(name,icon){
+	name.innerHTML=icon;
 }
-email.onmouseover=function()
-{
-	email.style.background="#F1B702";
-	email.style.color="#000000";
-	email.style.cursor="text";
-	email.style.borderRadius="5px";
-	email.innerHTML='isaac.qa13@gmail.com';
-}
-email.onmouseout=function(){
-	email.innerHTML='<img class="contacto" src="WebContent/icons/gmail.png"/>';
-	email.style.background="#F1B702";
-	email.style.cursor="pointer";
-}
-img.onclick = function(){
+/*
+img.onclick=function(){
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.alt;
 }
-span.onclick = function() { 
+span.onclick=function() { 
     modal.style.display = "none";
 }
+*/
