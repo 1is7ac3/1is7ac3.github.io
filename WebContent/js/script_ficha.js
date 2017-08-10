99536874e87f143de53f8b11f665f402267bbbc0
1is7ac3 <isaac.qa13@gmail.com>
2017-08-10 08:03:09 -0400

    update

--- WebContent/js/script_ficha.js
+++ WebContent/js/script_ficha.js
@@ -1,20 +1,25 @@
-var modal = document.getElementById('myModal');
-var text = document.getElementsByClassName("text")[0];
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-var span = document.getElementsByClassName("close")[0];
+var modal=document.getElementById('myModal');
+var text1=document.getElementById("text1");
+var text2=document.getElementById('text2');
+var modalImg=document.getElementById("img01");
+var captionText=document.getElementById("caption");
+var span=document.getElementsByClassName("close")[0];
 var foto=document.getElementById("foto");
-console.log(span);
-console.log(text);
-foto.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
+
+foto.onclick=function(){
+    modal.style.display="block";
+    modalImg.src=this.src;
+    captionText.innerHTML=this.alt;
 }
-text.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = "document/certificado_proccel.jpg";
-    captionText.innerHTML = "Certificado Proccel";
+text1.onclick=function(){
+    modal.style.display="block";
+    modalImg.src="document/certificado_proccel.jpg";
+    captionText.innerHTML="Certificado Proccel";
+}
+text2.onclick=function(){
+	modal.style.display='block';
+	modalImg.src='document/certificado_2.png';
+	captionText.innerHTML='Certificado Activate';
 }
 span.onclick = function() { 
     modal.style.display = "none";
