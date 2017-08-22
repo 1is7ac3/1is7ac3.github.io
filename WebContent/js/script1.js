c4419c0183ecb432bc2a28b093813130b916a006
1is7ac3 <isaac.qa13@gmail.com>
2017-08-22 08:56:59 -0400

    Updates 22-08-2017

--- WebContent/js/script1.js
+++ WebContent/js/script1.js
@@ -1,13 +1,12 @@
 $(window).on('load',function() {	
 	$('.close').hide();
-	$('#myModal').show('slow',function(){
+	$('#myModal').show('fast',function(){
 		$(this).fadeOut(5000);
-		modalImg.src=logo.src;
-				
+		$('#img01').attr('src',$('#logo').attr('src'));
 	});
-	
 });
 $(document).ready(function(){
+	console.log($('#logo').attr('src'));
 	$('#email').click(function(){
 		$('#e').toggle('slow');
 	});
@@ -17,27 +16,17 @@ $(document).ready(function(){
 	
 	$('#cajero').click(function(){
 		$('.close').show();
+		$('#myModal').show('slow');
+		$('#img01').attr('src',this.src);
+	});
+	$('#dibujo').click(function(){
+		$('.close').show();
+		$('#myModal').show('slow');
+		$('#img01').attr('src',this.src);
 	});
 	$('.close').click(function(){
 		$('#myModal').fadeOut(1000);
 	});
 });
-var modal=document.getElementById('myModal');
-var logo=document.getElementById('logo');
-var img1=document.getElementById('cajero');
-var img2=document.getElementById('dibujo');
-var modalImg=document.getElementById("img01");
-var captionText=document.getElementById("caption");
-var span=document.getElementsByClassName("close")[0];
 
-img1.onclick=function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
-img2.onclick=function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
 
